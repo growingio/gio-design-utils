3993cf152f6ecdde97205cb6352166be0d9b6a8a
Pan Jie <panjie@growingio.com>
2021-03-29 20:46:09 +0800

    chore: setup storybook and add home doc

---
+++ .storybook/main.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        sourceLoaderOptions: {
+          injectStoryParameters: false,
+        },
+      },
+    },
+    '@storybook/addon-controls',
+    '@storybook/addon-actions',
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        loaderOptions: {
+          prettierConfig: { printWidth: 80, singleQuote: false },
+        },
+      },
+    },
+    'storybook-addon-designs',
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.less$/,
+      use: ['style-loader', 'css-loader', 'less-loader'],
+    });
+
+    return config;
+  },
+};
