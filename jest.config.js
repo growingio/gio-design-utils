f9daa358421e4f2f368721ec9420e4d9d1e67f1d
Pan Jie <panjie@growingio.com>
2021-03-29 13:53:38 +0800

    chore: remove unused packages and update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,8 @@
 module.exports = {
-  // if you're also using typescript
-  preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
-  // registers babel.config.js with jest
   transform: {
     '^.+\\.js$': 'babel-jest',
-  },
-
-  // explicitly include any node libs using ESM modules
-  // "node_modules/?!(<ESM module here>|<another here>|<etc...>)"
-  // transformIgnorePatterns: ['node_modules/?!(@gio-design\/icon)', '!(@gio-design/icon)'],
-  transformIgnorePatterns: ['/node_modules/(?!@gio-design/icon).+\\.js$'],
-
-  setupFilesAfterEnv: ['<rootDir>/enzyme-adapter.js'],
-
-  moduleNameMapper: {
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    'iconfont.js': '<rootDir>/__mocks__/iconMock.js',
   },
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
@@ -30,6 +13,5 @@ module.exports = {
     '!src/**/__test?(s)__/*',
   ],
   coverageDirectory: './coverage/',
-  coveragePathIgnorePatterns: ['list/'],
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
 };
