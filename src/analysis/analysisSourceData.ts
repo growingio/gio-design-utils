03e59dbb0dad0c5e870af085a8cc1aea5af0b6af
Ryan Zhang <eneblue@126.com>
2021-10-09 11:20:47 +0800

    feat(analysisSourceData): add function about analysis chart data (#161)
    * feat(analysisSourceData): add function about analysis chart data
    
    * fix(lint): fix lint issues
    
    * fix(lint): fix lint issues
    
    * fix(test): update test case
    
    * fix(test): fix test issue
    
    Co-authored-by: Zhang Rui <zhangrui@growingio.com>

---
+++ src/analysis/analysisSourceData.ts
@@ -0,0 +1,35 @@
+import { LooseObject } from '../interfaces';
+
+const analysisRow = (chartType: string, data: LooseObject, column: LooseObject) =>
+  chartType === 'column' && column.id === 'tm' ? String(data) : data;
+
+export interface AnalysisOptions {
+  // chart的类型
+  chart?: string;
+  // 在生成的data中，需要补充的结构，比如color
+  fetch?: LooseObject;
+  // 格式化的方法，使用者根据formatter重新定义数据
+  formatter?: <T extends string | number>(value: T, column?: LooseObject) => T;
+  instead?: LooseObject;
+}
+
+export const analysisSourceData = (chartData: LooseObject, options?: AnalysisOptions) => {
+  const chartType = options?.chart || 'line';
+  const fetch = options?.fetch || {};
+  const instead = options?.instead || {};
+  const formatter = options?.formatter;
+
+  const source = chartData?.data || [];
+  const columns = chartData?.meta?.columns;
+
+  return source.map((item: LooseObject) => {
+    const row = {} as LooseObject;
+    columns?.forEach((column: LooseObject, index: number) => {
+      const defaultValue = item[index];
+      const value = formatter ? formatter(defaultValue, column) : analysisRow(chartType, defaultValue, column);
+      const rowId = instead[column.id] || column.id;
+      row[rowId] = value;
+    });
+    return Object.assign(row, fetch);
+  });
+};
