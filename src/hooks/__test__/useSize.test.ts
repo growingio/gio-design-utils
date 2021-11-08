a72ad248204bc489510da533f6abaebcf7361258
Zhou <zhouzhaoting@growingio.com>
2021-11-08 11:54:25 +0800

    fix bug

--- src/hooks/__test__/useSize.test.ts
+++
@@ -1,9 +0,0 @@
-import { renderHook } from '@testing-library/react-hooks';
-import useSize from '../useSize';
-
-describe('useSize', () => {
-  it('can be used without arguments', () => {
-    const { result } = renderHook(() => useSize());
-    expect(result.current).toEqual('middle');
-  });
-});
